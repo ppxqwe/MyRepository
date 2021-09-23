0e187012fd2e47b515518b35da9e6c1e3f83b752
ppxqwe <2453687466@qq.com>
2021-09-23 22:38:27 +0800

    第三次提交

--- Demo01/src/Test01.java
+++ Demo01/src/Test01.java
@@ -1,5 +1,6 @@
 public class Test01 {
     public static void main(String[] args) {
+        System.out.println("hhhhh");
         for(int i=1;i<10;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j+"*"+i+"="+i*j+"\t");
