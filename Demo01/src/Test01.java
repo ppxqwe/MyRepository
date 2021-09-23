b46c5591333d3f6fc6524f7c5242dbb5ff48cec9
ppxqwe <2453687466@qq.com>
2021-09-23 21:27:28 +0800

    第二次提交

--- Demo01/src/Test01.java
+++ Demo01/src/Test01.java
@@ -1,8 +1,8 @@
 public class Test01 {
     public static void main(String[] args) {
         for(int i=1;i<10;i++){
-            for(int j=1;j<i;j++){
-                System.out.print(i+"*"+j+"="+i*j+"\t");
+            for(int j=1;j<=i;j++){
+                System.out.print(j+"*"+i+"="+i*j+"\t");
             }
             System.out.println();
         }
