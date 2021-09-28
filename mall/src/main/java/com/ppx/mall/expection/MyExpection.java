4012df202d80ab1d0b5b8d44cb1ef9f38bf35bfd
ppxqwe <2453687466@qq.com>
2021-09-28 16:23:23 +0800

    创建项目

---
+++ mall/src/main/java/com/ppx/mall/expection/MyExpection.java
@@ -0,0 +1,16 @@
+package com.ppx.mall.expection;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class MyExpection{
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handler(Exception e){
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("error");
+        mv.addObject("message",e.getMessage());
+        return mv;
+    }
+}
